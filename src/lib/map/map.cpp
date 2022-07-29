76d535aedcdce4b1a41d4f325dbf2cb00356293b
Haz3 <haz3op@gmail.com>
2022-07-30 01:38:44 +0200

    aggiornata la mappa

---
+++ src/lib/map/map.cpp
@@ -0,0 +1,21 @@
+/*quali variabili mi potrebbero servire per una stanza:
+Coordinata X del giocatore
+Coordinata Y del giocatore
+Numero di Porte (magari delle flag per gestire quali porte ci sono e quali no)
+Numero di mob
+Numero di oggetti ecc
+*/
+
+
+
+
+class Room{
+    protected:
+    int xPosition;
+    int yPosition;
+    bool NorthDoor;
+    bool WestDoor;
+    bool EastDoor;
+    bool SouthDoor;
+    
+};
