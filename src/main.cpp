8ac6763c4a35c82d190ae9f842646f83dda37fe9
IHateLiam <liambusnelli02@gmail.com>
2022-07-31 16:31:28 +0100

    nieinte

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,15 @@
 int main(int argc, char **argv) {
     initscr();
     printw("Hello World !!!");	/* Print Hello World		  */
+
+    WINDOW myWin;
+
+    
+
+
+    box(stdscr, 0, 0);
+
+
 	refresh();			/* Print it on to the real screen */
 	getch();			/* Wait for user input */
 	endwin();	
