a6e8e858a8aa1115380fde81a023316aad076ff7
Haz3 <haz3op@gmail.commake>
2022-09-03 17:47:35 +0200

    iniziati i colori, da finire perchè non li fa vedere

--- lib/board.hpp
+++ lib/board.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <ncursesw/ncurses.h>
+#include <string>
+#include <map>
+using namespace std;
 
 /**
  * Author:
@@ -10,13 +13,19 @@
  */
 class Board
 {
-private:
+protected:
 	WINDOW *board_win;
 	int screen_height, screen_width;
+	map<string,int> m_colors;
+	string m_lastbackground, m_lastforeground;
 
 public:
 	// Board();
 	Board(int screen_height, int screen_width);
+	void setupColors();
+	int getColor(const string& background, const string& foreground);
+	void startColor(const string& background, const string& foreground);
+	void endColor();
 	void initialize();
 	void addBorder();
 	void clear();
--- src/board.cpp
+++ src/board.cpp
@@ -1,6 +1,4 @@
 #include "../lib/board.hpp"
-#include <string>
-using namespace std;
 
 /*Board::Board(){
 	int xMax, yMax;
@@ -12,8 +10,7 @@ using namespace std;
 	initialize();
 }*/
 
-Board::Board(int screen_height, int screen_width)
-{
+Board::Board(int screen_height, int screen_width){
 	int xMax, yMax;
 	
 	getmaxyx(stdscr, yMax, xMax);
@@ -21,13 +18,11 @@ Board::Board(int screen_height, int screen_width)
 	initialize();
 }
 
-void Board::refresh()
-{
+void Board::refresh(){
 	wrefresh(board_win);
 }
 
-void Board::addBorder()
-{
+void Board::addBorder(){
 	string fence = "#";
 	int xMax, yMax, Offset;
 	getmaxyx(stdscr, yMax, xMax);
@@ -36,24 +31,61 @@ void Board::addBorder()
 	yMax=yMax-Offset;
 	for(int fy = Offset; fy < yMax; fy++){
 		for(int fx = Offset; fx < xMax; fx++){
-			if(fx == Offset || fy == Offset || fx == xMax-1 || fy == yMax-1)
+			if(fx == Offset || fy == Offset || fx == xMax-1 || fy == yMax-1){
+				startColor("green", "green");
 				mvprintw(fy, fx, fence.c_str());
+				endColor();
+			}			
 		}
 	}
 }
 
-void Board::clear()
-{
+void Board::setupColors(){
+	/*Black = 0, Red = 1, Green = 2, Yellow = 3, Blue = 4, Magenta = 5, Cyan = 6, White = 7*/
+	string colorNames[] = {
+		"black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"
+	};
+	int counter = 0;
+	for (int fg = 0; fg < 8; fg++){
+		for (int bg = 0; fg < 8; bg ++){
+			string colorName = colorNames[bg] + "-" + colorNames[fg];
+			init_pair(counter, fg, bg);
+			m_colors[colorName] = counter;
+			counter++;
+		}
+	}
+}
+
+int Board::getColor(const string& background, const string& foreground){
+	string colorName = background + "-" + foreground;
+	return m_colors[colorName];
+}
+
+void Board::startColor(const string& background, const string& foreground){
+	if(m_lastbackground != ""){
+		endColor();
+	}
+	m_lastbackground = background;
+	m_lastforeground = foreground;
+	attron(COLOR_PAIR(getColor(background, foreground)));
+}
+
+void Board::endColor(){
+	attroff(COLOR_PAIR(getColor(m_lastbackground, m_lastforeground)));
+	m_lastbackground = "";
+	m_lastforeground = "";
+}
+
+void Board::clear(){
 	wclear(board_win);
 	addBorder();
 }
 
-void Board::initialize()
-{
+void Board::initialize(){
 	clear();
 	refresh();
 }
 
-WINDOW* Board::getWin() {
+WINDOW* Board::getWin(){
 	return board_win;
 }
