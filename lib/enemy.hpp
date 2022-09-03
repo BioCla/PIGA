400fea4eba3755cc3ee03e19bff3076c45f3bc75
manTo16 <fmantov5@gmail.com>
2022-09-03 02:26:15 +0200

    enemies

--- lib/enemy.hpp
+++ lib/enemy.hpp
@@ -1,6 +1,9 @@
+#include <ncursesw/ncurses.h>
+#include "../assets/position.hpp"
+
 class Enemy { // i vari tipi di nemici saranno sottoclasse di questa classe
     protected:
-        char icon;
+        const char* icon;
         int health;
         int max_health;
         int damage;       //danno che infligge il nemico al giocatore
--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,1 +1,26 @@
 #include "../lib/enemy.hpp"
+
+Enemy::Enemy(){
+    icon = "A";
+    health = 10;
+    max_health = 10;
+    damage = 2;
+    //altri parametri
+    // [creare più sottoclassi di nemici]
+}
+
+void Enemy::setHealth(int set){
+    health = set;
+}
+
+int Enemy::getHealth(){
+    return health;
+}
+
+void Enemy::setDamage(int set){
+    damage = set;
+}
+
+int Enemy::getDamage(){
+    return damage;
+}
