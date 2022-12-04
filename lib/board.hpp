deb9e58101f37860ea68741262a5449a109b3ce6
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2022-12-04 09:58:20 +0100

    asdflkjh

--- lib/board.hpp
+++ lib/board.hpp
@@ -8,33 +8,27 @@
 #include <stdlib.h>
 using namespace std;
 
-
 #define BOARD_DIM 35
 #define BOARD_ROWS BOARD_DIM
-#define BOARD_COLS (int) (BOARD_ROWS * 2.5)
+#define BOARD_COLS (int)(BOARD_ROWS * 2.5)
 
-#define WALL      '#'
-#define PAVE      ' '
-#define PLAYER    '@'
-#define ROCK      '^'
-#define ENEMY     'A'
-#define PROJCTL   '*'
+#define WALL		'#'
+#define PAVE		' '
+#define PLAYER		'@'
+#define ROCK		'^'
+#define ENEMY		'A'
+#define PROJCTL		'*'
 
-
-
-
-#define WALL_PAIR      1
-#define PAVE_PAIR      2
-#define PLAYER_PAIR    3
-#define ROCK_PAIR      4
-#define ENEMY_PAIR 	   5
-#define PROJCTL_PAIR   6
-#define PAVE_FOREGROUND 7
-#define WALL_FOREGROUND 8
-#define PLAYER_FOREGROUND 9
-#define ALL_FOREGROUND 10
-
-
+#define WALL_PAIR			1
+#define PAVE_PAIR			2
+#define PLAYER_PAIR			3
+#define ROCK_PAIR			4
+#define ENEMY_PAIR			5
+#define PROJCTL_PAIR		6
+#define PAVE_FOREGROUND		7
+#define WALL_FOREGROUND		8
+#define PLAYER_FOREGROUND	9
+#define ALL_FOREGROUND		10
 
 /**
  * Author:
@@ -50,12 +44,12 @@ protected:
 	WINDOW *board_win;
 	int screen_height, screen_width, xMin, yMin, xMax, yMax;
 
-	//questi due campi ignorali tranquillamente
-	//sono solo cose che usano altre classi e vengono salvate nella stanza
+	// questi due campi ignorali tranquillamente
+	// sono solo cose che usano altre classi e vengono salvate nella stanza
 	List<Projectile> projectilesList;
 	List<SuperProjectile> superProjectilesList;
 	List<Enemy> enemiesList;
-	
+
 public:
 	// Board();
 	Board(int screen_height, int screen_width);
@@ -66,16 +60,15 @@ public:
 	void fillPoint(int y, int x);
 	void clear();
 	void init();
-	WINDOW* getWin();
+	WINDOW *getWin();
 	void checkColor();
 
-
-	//funzioni dei campi che puoi ignorare
+	// funzioni dei campi che puoi ignorare
 	void checkHits();
 	void refreshEnemies(system_clock::time_point time_now, Position pos);
 	void generateEnemies();
 	void generateEnemiesProj(system_clock::time_point time_now);
-	List<Projectile>* getProjectilesList();
-	List<SuperProjectile>* getSuperProjectilesList();
-	List<Enemy>* getEnemiesList();
+	List<Projectile> *getProjectilesList();
+	List<SuperProjectile> *getSuperProjectilesList();
+	List<Enemy> *getEnemiesList();
 };
