0fbbd1251bf6ea8d769909c8143e373c28f8b830
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2023-01-02 14:29:05 +0100

    oops, main

--- src/_init/main.cpp
+++ src/_init/main.cpp
@@ -4,7 +4,7 @@
 #include "../../include/util/adj_graph_override.tpp"
 
 int main() {
-	/* init();
+	init();
 	init_colorpairs();
 	
 	//Testing Colors
@@ -22,30 +22,6 @@ int main() {
 		loop.start();
 	
 	
-	endwin(); */
-
-	// Testing Graphs
-		Graph<float> graph;
-		graph.AddVertex(0);
-		graph.AddVertex(1);
-		graph.AddVertex(2);
-		graph.AddVertex(3);
-
-		std::cout << graph[0] << std::endl;
-
-		graph[0] = 5.00002f;
-		std::cout << graph[0] << std::endl;
-
-		graph.RemoveVertex(3);
-
-		graph.AddEdge(0, 1);
-		graph.AddEdge(0, 2);
-		graph.AddEdge(1, 2);	
-
-		std::cout << graph << std::endl;
-
-		graph.RemoveEdge(0, 1);
-
-		std::cout << graph << std::endl;
+	endwin();
 	return 0;
 }
